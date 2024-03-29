90ac32f0ffd122e1b85558b0d227ed66c8fc3bfd
eavelie <athallahnaufalr@gmail.com>
2024-03-29 17:28:46 +0700

    added user logging

--- scripts/user.php
+++ scripts/user.php
@@ -6,4 +6,11 @@ if (!file_exists($filename)) {
     fopen($filename, "w");
 }
 
+$users = "../data/users.txt";
+if (!file_exists($users)) {
+    fopen($users, "w");
+}
+
+file_put_contents($users, $username . ",", FILE_APPEND);
+
 echo 'Username created succesfully';
